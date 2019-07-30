7248a9af6a3eec815826bef73d5e3b5c590af6c9
naixy28 <yuanyang@ruguoapp.com>
2019-07-30 13:26:35 +0800

    update

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.64d710a3a9f836c7c1c06807e34e48d1.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.36067cf8f0b71b9ec4810a00fb1c4f1e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable */
 workbox.core.setCacheNameDetails({prefix: "glitch-starter"});
