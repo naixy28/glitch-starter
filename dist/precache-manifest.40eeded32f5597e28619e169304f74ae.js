430ea4a42d1fcfded9d9ccaa253d5b4581640db9
naixy28 <yuanyang@ruguoapp.com>
2019-07-31 10:36:49 +0800

    update

--- dist/precache-manifest.40eeded32f5597e28619e169304f74ae.js
+++ dist/precache-manifest.40eeded32f5597e28619e169304f74ae.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "4a075f15d58620e17f5d306fe5057b0c",
-    "url": "/img/fake-n.4a075f15.jpg"
+    "revision": "e833cb5e10117f777a3b605871c7fe2b",
+    "url": "/img/icon-nm.e833cb5e.png"
   },
   {
-    "revision": "8142fe9ede98d3a9e6a4",
-    "url": "/css/app.3d458bba.css"
+    "revision": "5e4f03517fb6fa3c5a79",
+    "url": "/css/app.b79e1264.css"
   },
   {
     "revision": "f716b57237032a37b9de",
@@ -20,31 +20,31 @@ self.__precacheManifest = [
     "url": "/img/icon-db.7bcfe656.png"
   },
   {
+    "revision": "d112d59c694df0af8447ac559c2d2d11",
+    "url": "/img/main-bg2.d112d59c.jpg"
+  },
+  {
     "revision": "e51bec0fc7e9db051023afc3fe677c26",
     "url": "/img/fake-c.e51bec0f.jpg"
   },
   {
-    "revision": "e833cb5e10117f777a3b605871c7fe2b",
-    "url": "/img/icon-nm.e833cb5e.png"
+    "revision": "5e4f03517fb6fa3c5a79",
+    "url": "/js/app.779ba23a.js"
   },
   {
-    "revision": "8142fe9ede98d3a9e6a4",
-    "url": "/js/app.d513ec2d.js"
+    "revision": "4a075f15d58620e17f5d306fe5057b0c",
+    "url": "/img/fake-n.4a075f15.jpg"
   },
   {
     "revision": "3aaaa513f763f1962aea0f2ea4a707eb",
     "url": "/img/icon-wb.3aaaa513.png"
   },
   {
-    "revision": "d112d59c694df0af8447ac559c2d2d11",
-    "url": "/img/main-bg2.d112d59c.jpg"
-  },
-  {
     "revision": "c3c8dd30e6533c4acbb309558ea7c46a",
     "url": "/img/fake-m.c3c8dd30.jpg"
   },
   {
-    "revision": "7b5cac44bb32de217e3af7e66ec27d66",
+    "revision": "e967ebc441dbdd220e5630d353de1856",
     "url": "/index.html"
   },
   {
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.48aeacd26736c09131b95c10fad507b2.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.40eeded32f5597e28619e169304f74ae.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* eslint-disable */
 workbox.core.setCacheNameDetails({prefix: "glitch-starter"});
